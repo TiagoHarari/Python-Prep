8ea7874316a0209e78a8b86b1a9d5ba5c4d9b5e7
TiagoHarari <tiago.harari@gmail.com>
2024-01-13 15:40:26 -0300

    commit1

---
+++ M03_variablesydatos/resumenM03.py
@@ -0,0 +1,15 @@
+#una variable es una caja donde puedo almacenar tipos de datos (numeros, textos, etc).
+
+#las variables poseen un nombre identificador (ponerle nombre o etiqueta a la caja)
+
+#un ESPACIO DE MEMORIA que recibe un NOMBRE
+
+#tipos de datos 
+
+x=10
+y=-1
+
+
+
+type(y)
+bool(x)
